c12e75c8792b614f656f11cd045852f3a85f4c53
Yannick <tech@yalit.be>
2026-07-12 10:59:49 +0200

    feat : day 2026 07

---
+++ 2026/07/ff.py
@@ -0,0 +1,72 @@
+def solution_1(lines: list[str]):
+    pos = (0,0)
+    movements = lines[0][:len(lines[0])//2]
+    i = 2
+    target = tuple(map(int, lines[i].split(',')))
+    count = 0
+
+    for m in movements:
+        pos = move(pos, m)
+        if pos == target:
+            count += 1
+            i += 1
+            target = tuple(map(int, lines[i].split(',')))
+
+
+    return count
+
+
+
+def solution_2(lines: list[str]):
+    movements = lines[0]
+    i = 2
+    target = tuple(map(int, lines[i].split(',')))
+    snake = tuple([(0,0)])
+
+    for x,m in enumerate(movements):
+        pos = move(snake[0],m)
+        if pos in snake[:-1]:
+            break
+
+        if pos != target:
+            snake = (pos,)+snake[:-1]
+        
+        if pos == target:
+            i += 1
+            target = tuple(map(int, lines[i].split(',')))
+            snake = (pos,)+snake
+    
+    return len(snake) 
+
+
+
+def solution_3(lines: list[str]):
+    movements = lines[0]
+    i = 2
+    target = tuple(map(int, lines[i].split(',')))
+    snake = tuple([(0,0)])
+    count = 0
+
+    for _,m in enumerate(movements):
+        pos = move(snake[0],m)
+        if pos in snake[:-1]:
+            cut = snake[:-1].index(pos)
+            snake = snake[:cut]
+            count +=1
+
+        if pos != target:
+            snake = (pos,)+snake[:-1]
+        
+        if pos == target:
+            i += 1
+            if i < len(lines):
+                target = tuple(map(int, lines[i].split(',')))
+            else:
+                target = None
+            snake = (pos,)+snake
+    
+    return len(snake) * count
+
+def move(pos, m):
+    moves = {'>': (1,0), '<': (-1,0), '^': (0,1), 'v': (0,-1)}
+    return (pos[0]+moves[m][0], pos[1]+moves[m][1])
--- main.py
+++ main.py
@@ -20,24 +20,18 @@ dayScript = import_module(f"{year}.{day}.ff")
 
 if runType == "test":
     print(f"Running day {year}/{day} --- TEST")
-    file1 = open(f"{year}/{day}/input_test_1.txt", "r")
-    file2 = open(f"{year}/{day}/input_test_2.txt", "r")
-    file3 = open(f"{year}/{day}/input_test_3.txt", "r")
+    file = open(f"{year}/{day}/input_test_1.txt", "r")
 else:
     print(f"Running day {year}/{day}")
-    file1 = open(f"{year}/{day}/input_1.txt", "r")
-    file2 = open(f"{year}/{day}/input_2.txt", "r")
-    file3 = open(f"{year}/{day}/input_3.txt", "r")
+    file = open(f"{year}/{day}/input_1.txt", "r")
 
 
+lines = [x.strip("\n") for x in file.readlines()]
 if len(sys.argv) == 4 or sys.argv[4] == "1":
-    lines = [x.strip("\n") for x in file1.readlines()]
     print("Solution part 1 : ", dayScript.solution_1(lines))
 
 if len(sys.argv) == 4 or sys.argv[4] == "2":
-    lines = [x.strip("\n") for x in file2.readlines()]
     print("Solution part 2 : ", dayScript.solution_2(lines))
 
 if len(sys.argv) == 4 or sys.argv[4] == "3":
-    lines = [x.strip("\n") for x in file3.readlines()]
     print("Solution part 3 : ", dayScript.solution_3(lines))
